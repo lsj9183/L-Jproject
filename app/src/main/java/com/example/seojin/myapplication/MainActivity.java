2b58b8bf17e272ab595f99fca6b271bfb5392583
seojin <seojin@gmail.com>
2017-04-17 11:37:19 +0900

    메인 페이지 수정

--- app/src/main/java/com/example/seojin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/seojin/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.seojin.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button counselButton = (Button) findViewById(R.id.counselButton);
+        final Button listButton = (Button) findViewById(R.id.listButton);
+        final Button userIfoButton = (Button) findViewById(R.id.userIfoButton);
+
+        counselButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+            }
+        });
+
+        listButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+            }
+        });
+
+        userIfoButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+            }
+        });
     }
 }
