bcbc63ace6d6d7783c124a2975b358c00121f30d
nine9ash <nine9ash@gmail.com>
2017-06-21 17:26:02 +0900

    캘린더 업데이트

--- app/src/main/java/com/example/seojin/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/seojin/myapplication/RegisterActivity.java
@@ -12,23 +12,21 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
     private ArrayAdapter adapter;
     private Spinner spinner;
-    private String studentID;
-    private String studentName;
-    private String studentPassword;
-    private String studentPassword2;
-    private String studentGrade;
-    private String studentMajor;
+    private String userID;
+    private String userName;
+    private String userPassword;
+    private String getUserPassword2;
+    private String userGrade;
+    private String userMajor;
     private AlertDialog dialog;
-    private boolean validate = false;
+    private boolean validate = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,14 +44,14 @@ public class RegisterActivity extends AppCompatActivity {
 
         RadioGroup gradeGroup = (RadioGroup)findViewById(R.id.gradeGroup);
         int gradeGroupID = gradeGroup.getCheckedRadioButtonId();
-        studentGrade = ((RadioButton)findViewById(gradeGroupID)).getText().toString();
+        userGrade = ((RadioButton)findViewById(gradeGroupID)).getText().toString();
 
         gradeGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
 
             @Override
-            public void onCheckedChanged(RadioGroup group, int i) {
-                RadioButton gradeButton = (RadioButton)findViewById(i);
-                studentGrade = gradeButton.getText().toString();
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton gradeButton = (RadioButton)findViewById(checkedId);
+                userGrade = gradeButton.getText().toString();
             }
         });
 
@@ -62,11 +60,12 @@ public class RegisterActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                String studentID = idText.getText().toString();
-                if(validate){
-                    return;
-                }
-                if(studentID.equals("")){
+                String userID = idText.getText().toString();
+                //중복체크 부분은 DB문제로 true, false 값은 확인 안 하도록 함
+                //if(validate){dsa
+                //    return;
+                //}
+                if(userID.equals("")){
                     AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
                     dialog = builder.setMessage("아이디를 채워주세요.")
                             .setPositiveButton("확인",null)
@@ -106,9 +105,6 @@ public class RegisterActivity extends AppCompatActivity {
                         }
                     }
                 };
-                ValidateRequest valdateRequest = new ValidateRequest(studentID, responseListener);
-                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
-                queue.add(valdateRequest);
             }
         });
 
@@ -117,81 +113,47 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String studentID = idText.getText().toString();
-                String studentName = nameText.getText().toString();
-                String studentPassword = passwordText.getText().toString();
-                String studentPassword2 = passwordTextCheck.getText().toString();
-                String studentMajor = spinner.getSelectedItem().toString();
+                String userID = idText.getText().toString();
+                String userName = nameText.getText().toString();
+                String userPassword = passwordText.getText().toString();
+                String userPassword2 = passwordTextCheck.getText().toString();
+                String userMajor = spinner.getSelectedItem().toString();
 
 
                 if(!validate){
                     AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
                     dialog = builder.setMessage("중복체크를 해주세요.")
-                            .setNegativeButton("확인",null)
+                            .setPositiveButton("확인",null)
                             .create();
                     dialog.show();
                     return;
                 }
-                if (studentID.equals("") || studentPassword.equals("") || studentName.equals("") || studentMajor.equals("") || studentPassword2.equals("") || studentGrade.equals("")) {
+                if (userID.equals("") || userPassword.equals("") || userName.equals("") || userMajor.equals("") || userPassword2.equals("") || userGrade.equals("")) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
                     dialog = builder.setMessage("빈 칸없이 입력해주세요.")
                             .setNegativeButton("확인",null)
                             .create();
                     dialog.show();
-                    return;
                 }
 
 
-                if (studentPassword.toString().equals(studentPassword2) == false) {
+                else if (userPassword.toString().equals(userPassword2) == false) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
                     builder.setMessage("비밀번호가 다릅니다.")
                             .setNegativeButton("다시시도", null)
                             .create()
                             .show();
-                    return;
+                } else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    builder.setMessage("회원가입에 성공하였습니다.")
+                            .setPositiveButton("확인", null)
+                            .create()
+                            .show();
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    RegisterActivity.this.startActivity(intent);
                 }
 
-                Response.Listener<String> responseListener = new Response.Listener<String>(){
-
-                    @Override
-                    public void onResponse(String response) {
-                        try{
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-                            if(success){
-                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                dialog = builder.setMessage("회원 등록에 성공했습니다.")
-                                        .setPositiveButton("확인",null)
-                                        .create();
-                                dialog.show();
-                                finish();
-                            }
-                            else {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                dialog = builder.setMessage("회원등록에 실패했습니다.")
-                                        .setNegativeButton("확인",null)
-                                        .create();
-                                dialog.show();
-                            }
-                        }
-                        catch (Exception e){
-                            e.printStackTrace();
-                        }
-                    }
-                };
-                RegisterRequest registerRequest = new RegisterRequest(studentID, studentPassword,studentName, studentGrade,studentMajor,responseListener);
-                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
-                queue.add(registerRequest);
             }
         });
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-        if(dialog != null){
-            dialog.dismiss();
-            dialog = null;
-        }
     }
 }
